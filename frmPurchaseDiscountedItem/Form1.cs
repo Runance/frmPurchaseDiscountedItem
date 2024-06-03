3370eee715a027c24f08d2ca912ad0cca9e33600
System.out.print ("Runance") <rebeccaaguilar047@gmail.com>
2024-06-03 18:40:35 +0800

    Add project files.

---
+++ frmPurchaseDiscountedItem/Form1.cs
@@ -0,0 +1,37 @@
+﻿using Itemnamespace;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace frmPurchaseDiscountedItem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private DiscountedItem cs;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cs = new DiscountedItem(textBox1.Text, Convert.ToDouble(textBox3.Text), Convert.ToInt16(textBox4.Text), Convert.ToDouble(textBox2.Text));
+            label5.Text = cs.getTotalPrice().ToString();
+
+        }
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cs.setPayment(Convert.ToDouble(textBox5.Text));
+            label7.Text = cs.getChange().ToString();
+        }
+    }
+    
+}
---
+++ frmPurchaseDiscountedItem/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itemnamespace
+{
+    internal abstract class Item
+    {
+
+        protected int Quantity;
+        protected double Total_Price;
+        protected double item_price;
+        protected string item_name;
+
+        public abstract double getTotalPrice();
+        public abstract double getChange();
+
+        public abstract void setPayment(double amount);
+
+
+    }
+}
